de4596c3092b37a41c6ae6dd1dad52ad6d65b79a
Alex <alexander.kaschta@t-online.de>
2018-02-16 19:25:51 +0100

    Initial commit

---
+++ app/src/main/java/de/codeoverflow/frc/monsterscoutmanager/SplashActivity.java
@@ -0,0 +1,31 @@
+package de.codeoverflow.frc.monsterscoutmanager;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        //Check if all variables are given
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        boolean isFirstStart = sharedPref.getBoolean("FIRST_START", true);
+        if(isFirstStart){
+            //startActivity(new Intent(this, PreSetupActivity.class));
+            finish();
+        }
+        else {
+            //startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+
+    }
+}
