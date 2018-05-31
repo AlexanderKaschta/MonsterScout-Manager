f4d0421bf148de5b2a323fcb13e66c4f812e0788
Alex <alexander.kaschta@t-online.de>
2018-05-31 20:03:45 +0200

    Added BottomNavigationBar

---
+++ app/src/main/java/de/codeoverflow/frc/monsterscoutmanager/fragments/MainFragment.java
@@ -0,0 +1,32 @@
+package de.codeoverflow.frc.monsterscoutmanager.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import de.codeoverflow.frc.monsterscoutmanager.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
+
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+}
